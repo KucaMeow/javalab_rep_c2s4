aa94a3fcf1971658d9b687b5fbf377d99479144d
Stepan <stepan@ponomarev.pp.ru>
2020-04-04 17:24:56 +0300

    Fixes

--- Course_work/src/main/java/ru/itis/controllers/ErrorController.java
+++ Course_work/src/main/java/ru/itis/controllers/ErrorController.java
@@ -1,45 +1,25 @@
 package ru.itis.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+import ru.itis.services.ErrorsService;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Controller
 public class ErrorController {
 
+    @Autowired
+    ErrorsService errorsService;
+
     @RequestMapping(value = "errors", method = RequestMethod.GET)
     public ModelAndView renderErrorPage(HttpServletRequest httpRequest) {
 
         ModelAndView errorPage = new ModelAndView("errorPage");
-        String errorMsg = "";
-        int httpErrorCode = getErrorCode(httpRequest);
-
-        switch (httpErrorCode) {
-            case 400: {
-                errorMsg = "Http Error Code: 400. Неправильный запрос";
-                break;
-            }
-            case 401: {
-                errorMsg = "Http Error Code: 401. Не авторизован";
-                break;
-            }
-            case 404: {
-                errorMsg = "Http Error Code: 404. Страница не найдена";
-                break;
-            }
-            case 403: {
-                errorMsg = "Http Error Code: 500. Доступ запрещен";
-                break;
-            }
-            case 500: {
-                errorMsg = "Http Error Code: 500. Ошибка на сервере";
-                break;
-            }
-        }
-        errorPage.addObject("errorMsg", errorMsg);
+        errorPage.addObject("errorMsg", errorsService.getErrorMessage(getErrorCode(httpRequest)));
         return errorPage;
     }
 
