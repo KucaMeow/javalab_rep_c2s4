0367324802bb68a99759a6579f4fec729ccd47df
Stepan Ponomarev <stepan@ponomarev.pp.ru>
2020-05-03 22:34:37 +0300

    Message queue on SockJS homework #fixes

--- javalabhw/src/main/java/ru/itis/javalabhw/config/WebSocketConfig.java
+++ javalabhw/src/main/java/ru/itis/javalabhw/config/WebSocketConfig.java
@@ -5,7 +5,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.socket.config.annotation.EnableWebSocket;
 import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
 import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
-import ru.itis.javalabhw.handlers.AuthHandshakeHandler;
 import ru.itis.javalabhw.handlers.WebSocketHandler;
 
 @Configuration
@@ -14,14 +13,11 @@ public class WebSocketConfig implements WebSocketConfigurer {
 
     @Autowired
     WebSocketHandler webSocketHandler;
-    @Autowired
-    AuthHandshakeHandler authHandshakeHandler;
 
     @Override
     public void registerWebSocketHandlers(WebSocketHandlerRegistry webSocketHandlerRegistry) {
         webSocketHandlerRegistry
                 .addHandler(webSocketHandler, "/javalabqueue")
-                .setHandshakeHandler(authHandshakeHandler)
                 .setAllowedOrigins("*")
                 .withSockJS();
     }
